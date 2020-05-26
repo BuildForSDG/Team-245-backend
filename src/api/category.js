d5e0f1015a41cbd817bd3288bc3ecb509b50f998
halim <haleemyoosuph@gmail.com>
2020-05-26 18:07:09 +0100

    fix: removed redundant code docs

--- src/api/category.js
+++ src/api/category.js
@@ -4,22 +4,8 @@ import * as controllers from '../controllers/category';
 
 const Router = express.Router();
 
-/**  @route    GET api/v1/posts
- @desc     get categories
- @access   Public
- */
 Router.get('/', controllers.fetchCategories);
-
-/**  @route    POST api/v1/posts
- @desc     Create a category
- @access   Private
- */
 Router.post('/', validator, controllers.createCategory);
-
-/**  @route    DELETE api/v1/posts/:id
- @desc     delete a category
- @access   Private
- */
 Router.delete('/:id', controllers.deleteCategory);
 
 export default Router;

4377d32ed25c65da8ff1a70c1e1f619ea49bc1fa
halim <haleemyoosuph@gmail.com>
2020-06-04 13:06:48 +0100

    fix: removed default app startup tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,10 +4,4 @@ describe('app module', () => {
   test('it exists', async () => {
     expect(app).toBeDefined();
   });
-
-  test('it returns program name with SDGs', async () => {
-    const result = await app();
-    const sdgPos = (result || '').indexOf('SDG');
-    expect(sdgPos).toBeGreaterThanOrEqual(0);
-  });
 });

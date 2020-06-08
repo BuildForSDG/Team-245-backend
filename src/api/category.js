82f4d479f4bf9d9da923ef65c4692f5d1f30824b
halim <haleemyoosuph@gmail.com>
2020-06-08 23:41:17 +0100

    chore:  add auth and admin auth middlewares to routes

--- src/api/category.js
+++ src/api/category.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import validator from '../validation/category';
+import auth from '../middlewares/validateUser';
+import adminAuth from '../middlewares/validateAdmin';
 import * as controllers from '../controllers/category';
 
 const Router = express.Router();
 
 Router.get('/', controllers.fetchCategories);
-Router.post('/', validator, controllers.createCategory);
-Router.delete('/:id', controllers.deleteCategory);
+Router.post('/', auth, adminAuth, validator, controllers.createCategory);
+Router.delete('/:id', auth, adminAuth, controllers.deleteCategory);
 
 export default Router;
--- src/api/post.js
+++ src/api/post.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import validator from '../validation/post';
+import auth from '../middlewares/validateUser';
 import * as controllers from '../controllers/post';
 
 const Router = express.Router();
 
 Router.get('/', controllers.fetchPosts);
 Router.get('/:id', controllers.fetchPost);
-Router.post('/', validator, controllers.createPost);
-Router.patch('/:id', validator, controllers.editPost);
-Router.delete('/:id', controllers.deletePost);
+Router.post('/', validator, auth, controllers.createPost);
+Router.patch('/:id', validator, auth, controllers.editPost);
+Router.delete('/:id', auth, controllers.deletePost);
 
 export default Router;

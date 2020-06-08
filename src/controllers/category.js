ab24ea55587e2f3e047ad9be2d2cf5afe9be1a2f
halim <haleemyoosuph@gmail.com>
2020-06-08 23:42:08 +0100

    chore: implement all category controllers

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,11 +1,15 @@
 import wrapper from '../utils/wrapper';
+import Category from '../models/category';
 import validatorErr from '../utils/validatorErr';
 
 /**  @route    GET api/v1/posts
  @desc     Get categories
  @access   Public
  */
-export const fetchCategories = wrapper(async () => {});
+export const fetchCategories = wrapper(async (_req, res) => {
+  const categories = await Category.find();
+  res.json({ data: categories });
+});
 
 /**  @route    POST api/v1/posts
  @desc     Create a category
@@ -14,12 +18,20 @@ export const fetchCategories = wrapper(async () => {});
 export const createCategory = wrapper(async (req, res) => {
   const err = validatorErr(req, res);
   if (err !== null) return err.response;
-  //   code here
-  return null;
+  req.body = { name: req.body.name };
+  let category = new Category(req.body);
+  category = await category.save();
+  return res.json({ data: category });
 });
 
 /**  @route    DELETE api/v1/posts/:id
  @desc     Delete a category
  @access   Private
  */
-export const deleteCategory = wrapper(async () => {});
+export const deleteCategory = wrapper(async (req, res) => {
+  const category = await Category.findByIdAndRemove(req.params.id);
+  if (category !== null) {
+    return res.json({ data: { message: 'Category deleted successfully' } });
+  }
+  return res.status(404).json({ error: 'Category not found' });
+});
